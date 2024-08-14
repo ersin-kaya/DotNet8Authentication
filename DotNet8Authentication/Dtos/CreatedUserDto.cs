f6daddf876771337b40065001d176341d8a477e3
ersin <ersin-kaya@outlook.com.tr>
2024-08-14 23:50:52 +0300

    Added RegisterResponseDto and CreatedUserDto

---
+++ DotNet8Authentication/Dtos/CreatedUserDto.cs
@@ -0,0 +1,9 @@
+namespace DotNet8Authentication.Dtos;
+
+public class CreatedUserDto<TId>
+{
+    public TId Id { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string FullName { get; set; }
+}
---
+++ DotNet8Authentication/Dtos/RegisterResponseDto.cs
@@ -0,0 +1,11 @@
+using DotNet8Authentication.Models;
+
+namespace DotNet8Authentication.Dtos;
+
+public class RegisterResponseDto
+{
+    public bool Succeeded { get; set; }
+    public string? Message { get; set; }
+    public IEnumerable<string>? ErrorMessages { get; set; }
+    public CreatedUserDto<string>? CreatedUser { get; set; }
+}
