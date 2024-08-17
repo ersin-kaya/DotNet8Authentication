900a4a99e8ef74491ebb293f1f72892985cc4bc6
ersin <ersin-kaya@outlook.com.tr>
2024-08-17 22:28:02 +0300

    Initialized ErrorMessages in RegisterResponseDto with an empty array

--- DotNet8Authentication/Dtos/RegisterResponseDto.cs
+++ DotNet8Authentication/Dtos/RegisterResponseDto.cs
@@ -6,7 +6,7 @@ public class RegisterResponseDto
 {
     public bool Succeeded { get; set; }
     public string? Message { get; set; }
-    public IEnumerable<string>? ErrorMessages { get; set; }
+    public IEnumerable<string>? ErrorMessages { get; set; } = [];
     public CreatedUserDto<string>? CreatedUser { get; set; }
     public UserToken? UserToken { get; set; }
 }
