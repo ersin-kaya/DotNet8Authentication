efd76a1fd6804bcc0dcf513b08e2f85713c394d8
ersin <ersin-kaya@outlook.com.tr>
2024-08-19 00:01:42 +0300

    Created ValidationConstants and refactored RegisterModel and LoginModel to use it

--- DotNet8Authentication/Models/LoginModel.cs
+++ DotNet8Authentication/Models/LoginModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using DotNet8Authentication.Constants;
 
 namespace DotNet8Authentication.Models;
 
@@ -9,6 +10,6 @@ public class LoginModel
     public string Email { get; set; }
     
     [Required]
-    [StringLength(32, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [StringLength(ValidationConstants.PasswordMaxLength, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = ValidationConstants.PasswordMinLength)]
     public string Password { get; set; }
 }
--- DotNet8Authentication/Models/RegisterModel.cs
+++ DotNet8Authentication/Models/RegisterModel.cs
@@ -1,11 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using DotNet8Authentication.Constants;
 
 namespace DotNet8Authentication.Models;
 
 public class RegisterModel
 {
     [Required]
-    [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+    [StringLength(ValidationConstants.UserNameMaxLength, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = ValidationConstants.UserNameMinLength)]
     public string UserName { get; set; }
     
     [Required]
@@ -13,13 +14,13 @@ public class RegisterModel
     public string Email { get; set; }
     
     [Required]
-    [StringLength(32, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [StringLength(ValidationConstants.PasswordMaxLength, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = ValidationConstants.PasswordMinLength)]
     public string Password { get; set; }
     
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
     public string ConfirmPassword { get; set; }
     
     [Required]
-    [StringLength(64, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
+    [StringLength(ValidationConstants.FullNameMaxLength, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = ValidationConstants.FullNameMinLength)]
     public string FullName { get; set; }
 }
