00111cd44c859ebe121f0da8029489f9681d8e83
ersin <ersin-kaya@outlook.com.tr>
2024-08-19 13:22:45 +0300

    Updated Messages.cs

--- DotNet8Authentication/Constants/Messages.cs
+++ DotNet8Authentication/Constants/Messages.cs
@@ -2,8 +2,13 @@ namespace DotNet8Authentication.Constants;
 
 public static class Messages
 {
-    public const string RegistrationSuccess = "Registration successful.";
-    public const string RegistrationError = "There was a problem with your registration. Please check your details and try again.";
-    
-    public const string EmailAlreadyInUse = "This email address is already in use.";
+    // Registration messages
+    public const string RegistrationSuccess = "Registration completed successfully.";
+    public const string RegistrationError = "An error occurred during registration.";
+    public const string EmailAlreadyInUse = "The email address is already in use.";
+
+    // Login messages
+    public const string LoginSuccess = "Login successful.";
+    public const string LoginError = "An error occurred during login.";
+    public const string InvalidLoginAttempt = "Invalid email or password.";
 }
