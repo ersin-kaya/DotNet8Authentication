68a694e1f5c85feb6b2d55af18fb690112969078
ersin <ersin-kaya@outlook.com.tr>
2024-08-20 03:24:31 +0300

    Refactored role creation to use Parallel.ForEachAsync for improved parallel processing

--- DotNet8Authentication/Program.cs
+++ DotNet8Authentication/Program.cs
@@ -58,11 +58,7 @@ builder.Services.AddSingleton<ISettingsService, SettingsService>();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    await EnsureRolesCreatedAsync(roleManager);
-}
+await EnsureRolesCreatedAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -99,7 +95,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-async Task EnsureRolesCreatedAsync(RoleManager<IdentityRole> roleManager)
+async Task EnsureRolesCreatedAsync()
 {
     var roles = new[]
     {
@@ -107,13 +103,55 @@ async Task EnsureRolesCreatedAsync(RoleManager<IdentityRole> roleManager)
         RoleConstants.User
     };
 
-    foreach (var role in roles)
+    #region Task.WhenAll
+
+    // The 'foreach' loop processes each role sequentially, one after the other.
+    // Task.WhenAll allows processing all roles concurrently, improving performance by handling them simultaneously.
+    // await Task.WhenAll(
+    //     roles.Select(async role =>
+    //         {
+    //             using (var scope = app.Services.CreateScope())
+    //             {
+    //                 var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    //
+    //                 if (!await roleManager.RoleExistsAsync(role))
+    //                 {
+    //                     await roleManager.CreateAsync(new IdentityRole(role));
+    //                 }
+    //                 
+    //                 // A concurrency error occurred when parallel tasks were executed using Task.WhenAll,
+    //                 // causing the same DbContext instance to be used by multiple threads simultaneously.
+    //                 // To resolve this, a new ServiceScope should be created for each parallel task,
+    //                 // allowing the RoleManager and consequently the DbContext instance to be obtained from this scope.
+    //                 // This approach ensures that each task uses an independent DbContext instance, preventing the error.
+    //             }
+    //         })
+    // );
+
+    #endregion
+
+    #region Parallel.ForEachAsync
+
+    // Parallel.ForEachAsync is an asynchronous structure that executes tasks in parallel at the same time,
+    // while foreach is synchronous and processes tasks sequentially.
+    await Parallel.ForEachAsync(roles, async (role, cancellationToken) =>
     {
-        if (!await roleManager.RoleExistsAsync(role))
+        using (var scope = app.Services.CreateScope())
         {
-            await roleManager.CreateAsync(new IdentityRole(role));
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
         }
-    }
+    });
+
+    #endregion
+    
+    // Parallel.ForEachAsync processes the elements of a collection in parallel and asynchronously,
+    // while Task.WhenAll runs independent asynchronous tasks together and waits for all of them to complete.
+    // Parallel.ForEachAsync is used for processing collections, while Task.WhenAll is used to start tasks in parallel.
 }
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
