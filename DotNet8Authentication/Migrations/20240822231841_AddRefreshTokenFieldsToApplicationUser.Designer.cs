b431b720a5d180a227ea118613b50bb130a4e863
ersin <ersin-kaya@outlook.com.tr>
2024-08-23 02:23:16 +0300

    Added migration: AddRefreshTokenFieldsToApplicationUser

--- DotNet8Authentication/Migrations/20240822231841_AddRefreshTokenFieldsToApplicationUser.Designer.cs
+++ DotNet8Authentication/Migrations/20240822231841_AddRefreshTokenFieldsToApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using DotNet8Authentication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNet8Authentication.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240822231841_AddRefreshTokenFieldsToApplicationUser")]
+    partial class AddRefreshTokenFieldsToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
