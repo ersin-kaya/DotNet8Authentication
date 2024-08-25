3a7451255f51a5fcedb64a326d1ae2ba19177c7c
ersin <ersin-kaya@outlook.com.tr>
2024-08-26 01:34:58 +0300

    Refactored Response constructor to use standard HTTP status codes

--- DotNet8Authentication/ResponseTypes/Response.cs
+++ DotNet8Authentication/ResponseTypes/Response.cs
@@ -1,17 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace DotNet8Authentication.ResponseTypes;
 
 public class Response : IResponse
 {
-    public Response(bool succeeded, string message = null, int statusCode = 999)
+    public Response(bool succeeded, string message = null, int? statusCode = null)
     {
         Succeeded = succeeded;
         Message = message;
-        StatusCode = (statusCode != 999)
-            ? statusCode
-            : (succeeded ? 1000 : 1500);
+        StatusCode = statusCode ?? (succeeded ? 200 : 500);
     }
 
     public bool Succeeded { get; }
     public string Message { get; }
+    
+    [JsonIgnore]
     public int StatusCode { get; }
 }
