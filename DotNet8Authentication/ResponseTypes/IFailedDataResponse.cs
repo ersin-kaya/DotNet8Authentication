0af525a69df16352a3bb9064d28e54b19cf21bf3
ersin <ersin-kaya@outlook.com.tr>
2024-08-27 01:32:08 +0300

    Updated IFailedDataResponse<TData, TError> interface and added IFailedDataResponse<TData>

--- DotNet8Authentication/ResponseTypes/IFailedDataResponse.cs
+++ DotNet8Authentication/ResponseTypes/IFailedDataResponse.cs
@@ -1,5 +1,10 @@
 namespace DotNet8Authentication.ResponseTypes;
 
+public interface IFailedDataResponse<TData> : IDataResponse<TData>, IFailedResponse
+{
+    
+}
+
 public interface IFailedDataResponse<TData, TError> : IDataResponse<TData>, IFailedResponse<TError>
 {
     
