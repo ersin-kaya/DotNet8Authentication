b43f7c1fa4cd17c336726bfd9baa45b106aac59c
ersin <ersin-kaya@outlook.com.tr>
2024-08-28 23:52:37 +0300

    Updated SuccessfulDataResponse<T> to set default value for data and fixed Succeeded to true

--- DotNet8Authentication/ResponseTypes/SuccessfulDataResponse.cs
+++ DotNet8Authentication/ResponseTypes/SuccessfulDataResponse.cs
@@ -2,7 +2,7 @@ namespace DotNet8Authentication.ResponseTypes;
 
 public class SuccessfulDataResponse<T> : DataResponse<T>, ISuccessfulDataResponse<T> 
 {
-    public SuccessfulDataResponse(T? data, bool succeeded, string? message = null, int? statusCode = null) : base(data, succeeded, message, statusCode)
+    public SuccessfulDataResponse(T? data = default, string? message = null, int? statusCode = null) : base(true, data, message, statusCode)
     {
         
     }
