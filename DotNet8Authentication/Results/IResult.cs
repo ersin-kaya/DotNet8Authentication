ac2037411df1fc15307dd056528317769c3d1f5e
ersin <ersin-kaya@outlook.com.tr>
2024-08-30 21:39:47 +0300

    Updated IResult interface: made ErrorMessages non-nullable

--- DotNet8Authentication/Results/IResult.cs
+++ DotNet8Authentication/Results/IResult.cs
@@ -4,5 +4,5 @@ public interface IResult
 {
     bool Succeeded { get; }
     string? Message { get; }
-    IEnumerable<string>? ErrorMessages { get; }
+    IEnumerable<string> ErrorMessages { get; }
 }
