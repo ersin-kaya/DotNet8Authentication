1fb3272b13e5c040a7a010d2c88c97087e7609f3
ersin <ersin-kaya@outlook.com.tr>
2024-09-08 15:20:54 +0300

    Created ApplicationUserDto

---
+++ CustomIdentityAuth/Dtos/ApplicationUserDto.cs
@@ -0,0 +1,10 @@
+namespace CustomIdentityAuth.Dtos;
+
+public class ApplicationUserDto
+{
+    public string Id { get; set; }
+    public string FullName { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
