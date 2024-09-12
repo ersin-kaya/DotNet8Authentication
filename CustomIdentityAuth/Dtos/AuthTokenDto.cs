2d2119213efc1a793502d2800c0725e19e156148
ersin <ersin-kaya@outlook.com.tr>
2024-09-12 23:56:33 +0300

    Updated AuthTokenDto to use Newtonsoft.Json attributes for null value handling

--- CustomIdentityAuth/Dtos/AuthTokenDto.cs
+++ CustomIdentityAuth/Dtos/AuthTokenDto.cs
@@ -1,15 +1,19 @@
 using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace CustomIdentityAuth.Dtos;
 
 public class AuthTokenDto
 {
     [JsonPropertyName("access_token")]
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public string AccessToken { get; set; }
 
     [JsonPropertyName("refresh_token")]
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public string RefreshToken { get; set; }
     
     [JsonPropertyName("expires_at")]
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public DateTimeOffset ExpiresAt { get; set; }
 }
