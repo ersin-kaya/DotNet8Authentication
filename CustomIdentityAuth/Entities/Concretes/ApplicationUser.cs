c3cc8ac27f6ea4203c00ab8cf0093d1de11f933f
ersin <ersin-kaya@outlook.com.tr>
2024-09-16 22:09:12 +0300

    Updated ApplicationUser

--- CustomIdentityAuth/Entities/Concretes/ApplicationUser.cs
+++ CustomIdentityAuth/Entities/Concretes/ApplicationUser.cs
@@ -4,15 +4,24 @@ namespace CustomIdentityAuth.Entities.Concretes;
 
 public class ApplicationUser : IdentityUser
 {
-    public string FullName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string? RefreshToken { get; set; }
     public DateTime? RefreshTokenExpiration { get; set; }
+    public DateTime LastActivity { get; set; }
     
+    public DateTime CreatedDate { get; private set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+    public bool IsActive { get; set; } = true;
+
     public ApplicationUser()
     {
+        CreatedDate = DateTime.UtcNow;
     }
 
     public ApplicationUser(string userName) : base(userName)
     {
+        CreatedDate = DateTime.UtcNow;
     }
 }
