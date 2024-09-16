1bf599a30e5b809ed382b31749c0d3039413e403
ersin <ersin-kaya@outlook.com.tr>
2024-09-16 22:14:38 +0300

    Updated ApplicationUserDto

--- CustomIdentityAuth/Dtos/Concretes/ApplicationUserDto.cs
+++ CustomIdentityAuth/Dtos/Concretes/ApplicationUserDto.cs
@@ -5,8 +5,10 @@ namespace CustomIdentityAuth.Dtos.Concretes;
 public class ApplicationUserDto : IDto
 {
     public string Id { get; set; }
-    public string FullName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
+    public DateTime LastActivity { get; set; }
 }
