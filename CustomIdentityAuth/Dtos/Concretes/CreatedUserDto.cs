c53579dba83f72d84c3803eee6e6af9de6cd90c1
ersin <ersin-kaya@outlook.com.tr>
2024-09-16 22:15:43 +0300

    Updated CreatedUserDto

--- CustomIdentityAuth/Dtos/Concretes/CreatedUserDto.cs
+++ CustomIdentityAuth/Dtos/Concretes/CreatedUserDto.cs
@@ -7,6 +7,8 @@ public class CreatedUserDto<TId> : IDto
     public TId Id { get; set; }
     public string Email { get; set; }
     public string UserName { get; set; }
-    public string FullName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime LastActivity { get; set; }
     public IList<string> Roles { get; set; }
 }
